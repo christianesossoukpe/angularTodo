9ede23ac55d2df1d1dcbe7cec4711b90c3fb6bed
Chirac Michoagan <michoaganchiracdona@gmail.com>
2024-08-09 10:33:32 +0100

    tableau des list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosListComponent } from './todos-list/todos-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosListComponent
   ],
   imports: [
     BrowserModule,
